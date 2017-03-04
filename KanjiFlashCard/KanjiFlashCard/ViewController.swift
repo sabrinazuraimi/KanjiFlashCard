ad7c1aadbef97035d27032fbb166eab7154ddb91
sabrina <sabrinazuraimi@outlook.com>
2017-03-04 11:37:31 +0900

    Initialize project

---
+++ KanjiFlashCard/KanjiFlashCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KanjiFlashCard
+//
+//  Created by Nur Sabrina binti Zuraimi on 2017/03/04.
+//  Copyright © 2017 Nur Sabrina binti Zuraimi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
